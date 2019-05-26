bd1e7f55b091436aa55b696bb4dcebdd5c7fd837
possess0602 <48153269+possess0602@users.noreply.github.com>
2019-05-26 17:31:04 +0800

    add product_API add create_product_1 add create_product_2是商品流程(輸入書的副類別) add create_product_2_furniture是商品流程(輸入家具副類別) add create_product_makesure是商品流程(確認先前輸入的資料)

--- app/src/main/java/com/example/FJU_Store/create_product_2.java
+++ app/src/main/java/com/example/FJU_Store/create_product_2.java
@@ -3,12 +3,11 @@ package com.example.FJU_Store;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class create_product_1 extends AppCompatActivity {
-
+public class create_product_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_product_1);
+        setContentView(R.layout.activity_create_product_2);
     }
 }
