f2147499d50341899b190c5c97b0b6b97efa61d2
possess0602 <48153269+possess0602@users.noreply.github.com>
2019-05-31 23:35:04 +0800

    makesure1 完成 intent 未傳值 Product.java 內容大致完成 圖片不知道如何定義 下一步要寫 流程間的傳值

--- app/src/main/java/com/example/FJU_Store/create_product_makesure.java
+++ app/src/main/java/com/example/FJU_Store/create_product_makesure.java
@@ -1,7 +1,9 @@
 package com.example.FJU_Store;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class create_product_makesure extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class create_product_makesure extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_product_makesure);
+    }
+
+    public void goto_create_product_makesure1(View v){
+        Intent it =new Intent(this,create_product_makesure1.class);
+        startActivity(it);
     }
 }
--- app/src/main/java/com/example/FJU_Store/product_API.java
+++ app/src/main/java/com/example/FJU_Store/product_API.java
@@ -1,5 +1,17 @@
 package com.example.FJU_Store;
 
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.QueryMap;
+
 public interface product_API {
+    @GET("Product")//User?api_key=dowefijiejfw
+    Call<ListRes> getProduct(@QueryMap(encoded = true) Map<String, String> queryMap);
+
+    @POST("Prodoct")
+    Call<ListRes> postProduct(@QueryMap(encoded = true) Map<String, String> queryMap);
 //
 }
